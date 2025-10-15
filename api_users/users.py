d1e849509413118b388246fd4ca965823215f623
AlbaDuqueC <alba.duque@iesnervion.es>
2025-10-15 10:12:26 +0200

    Servicios y Porcesos: Tema 1 API

---
+++ api_users/users.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class User(BaseModel):
+    id: int
+    name: str
+    surname: str
+    age: int
+
+users_list = [
+    User(id=1, name="Paco", surname="Perez", age=30),
+    User(id=2, name="María", surname="Martinez", age=20),
+    User(id=3, name="José", surname="Iglesia", age=22)
+]
+
+@app.get("/users")
+def users():
+    return users_list
+
+@app.get("/users/{id_user}")
+def get_user(id_user:int):
+    users = [user for user in users_list if user.id == id_user]
+
+    if(len(users))!=0:
+        return users[0]
+    else:
+        return{"error" : "No user found"}
