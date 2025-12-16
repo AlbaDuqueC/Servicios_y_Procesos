437c676351ddbd44369f41f230e7e94d49aef04b
AlbaDuqueC <alba.duque@iesnervion.es>
2025-12-16 14:05:45 +0100

    main corregido

--- ExamenAPI-AlbaDuque/main.py
+++ ExamenAPI-AlbaDuque/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from router import alumnos, colegios, auth_users, alumnos_db, colegio_bd
+from router import alumnos, colegios, auth_users, alumnos_db, colegio_db
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
@@ -9,4 +9,4 @@ app.include_router(alumnos.router)
 app.include_router(colegios.router)
 app.include_router(auth_users.router)
 app.include_router(alumnos_db.router)
-app.include_router(colegio_bd.router)
+app.include_router(colegio_db.router)
